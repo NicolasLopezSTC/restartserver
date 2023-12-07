22d22ea53c8181488631670ff9ce53bddfe573b6
NicolasLopezSTC <gclan149@gmail.com>
2023-12-07 00:32:59 -0600

    long trek ahead
    Signed-off-by: NicolasLopezSTC <gclan149@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,28 +1,14 @@
-import './utils/config'
-import express from 'express'
-import morgan from 'morgan'
-import cors from 'cors'
-import helmet from 'helmet'
-import bodyParser from 'body-parser'
-import basicAuth from 'express-basic-auth'
+import './utils/dotenv.js'
 
-import logger from './utils/logger'
-import router from './routes'
-import {notFound, errorHandler} from './utils/errors'
-const port = Number(process.env.PORT) || 3000
+import express from 'express'
+
 const app = express()
 
+const port = parse(process.env.PORT, 10) || 3000
 
-
-app.use(morgan(process.env.MORGAN_LOG))
-app.use(cors({origin: process.env.CORS_ORIGIN, exposedHeaders: [ 'x-total-count', 'x-total-pages'] }))
-app.use(helmet())
-app.use(bodyParser.json())
-app.use('/', router)
-
-app.use(notFound)
-app.use(errorHandler)
-
-app.listen(port, () => {
-    logger.info(`Server running on port ${port}`)
+app.get('/', (req, res) => {
+    const title = process.env.TITLE || 'finalserver2'
+ res.send({ msg: title })
 })
+
+app.listen(port)

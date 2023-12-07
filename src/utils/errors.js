7e6bf349f1737b2a9b8c5328571ee33235588832
NicolasLopezSTC <gclan149@gmail.com>
2023-12-07 01:44:54 -0600

    small error change
    Signed-off-by: NicolasLopezSTC <gclan149@gmail.com>

--- src/utils/errors.js
+++ src/utils/errors.js
@@ -1,7 +1,7 @@
 import logger from './logger'
 
 
-export const notFound = (error, req, res, next) => {
+export const notFound = (req, res, next) => {
   const error = new Error(`Not Found - ${req.originalUrl}`)
   res.status(404)
   next(error)

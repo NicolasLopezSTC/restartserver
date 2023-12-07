82567dee70f23b3a9c1c260ff2ba62ffedb6ed58
NicolasLopezSTC <gclan149@gmail.com>
2023-12-07 01:52:05 -0600

    fixed errors(again)
    Signed-off-by: NicolasLopezSTC <gclan149@gmail.com>

--- src/utils/errors.js
+++ src/utils/errors.js
@@ -1,17 +1,16 @@
 import logger from './logger'
 
-
 export const notFound = (req, res, next) => {
   const error = new Error(`Not Found - ${req.originalUrl}`)
   res.status(404)
   next(error)
 }
 
+// eslint-disable-next-line no-unused-vars
 export const errorHandler = (error, req, res, next) => {
   const statusCode = res.statusCode === 200 ? 500 : res.statusCode
 
-  logger.error( message: new Error(error.message))
-   
+  logger.error(new Error(error.message))
 
   res.status(statusCode)
   res.json({
@@ -19,4 +18,3 @@ export const errorHandler = (error, req, res, next) => {
     stack: process.env.NODE_ENV === 'production' ? '💩' : error.stack,
   })
 }
-

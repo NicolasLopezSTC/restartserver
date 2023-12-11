679ab68216bf9c7784139c60e74339fe9b3117ac
NicolasLopezSTC <gclan149@gmail.com>
2023-12-11 02:50:52 -0600

    final video
    Signed-off-by: NicolasLopezSTC <gclan149@gmail.com>

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import morgan from 'morgan'
 import cors from 'cors'
 import helmet from 'helmet'
 import bodyParser from 'body-parser'
-import basicAuth from 'express-basic-auth'
+
 import logger from './utils/logger'
 import router from './routes'
 //import { notFound, errorHandler } from './utils/errors'
@@ -14,13 +14,9 @@ const port = Number(process.env.PORT) || 3000
 
 const app = express()
 
-app.use(
-  basicAuth({
-    users: { [process.env.ADMIN_USER]: process.env.ADMIN_PASSWORD },
-  }),
-  )
+
 app.use(morgan(process.env.MORGAN_LOG))
-app.use(cors({ origin: process.env.ORIGIN }))
+app.use(cors({ origin: process.env.ORIGIN, exposedHeaders: ['x-total-count', 'x-total-pages'] }))
 app.use(helmet())
 app.use(bodyParser.json())
 
--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -1,4 +1,5 @@
 import { Router } from 'express'
+import basicAuth from 'express-basic-auth'
 
 import persons from './persons'
 import companies from './companies'
@@ -7,9 +8,25 @@ import employees from './employees'
 
 const router = Router()
 
+router.use(
+  basicAuth({
+    users: { [process.env.ADMIN_USER]: process.env.ADMIN_PASSWORD },
+  }),
+  )
+
+
+
+
 router.get('/', (req, res) => {
   res.send({ msg: 'Inside API Endpoints' })
 })
+
+
+
+
+
+
+
 
 router.use('/persons', persons)
 router.use('/companies', companies)
